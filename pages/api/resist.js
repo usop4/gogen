10e16f7aa6a870d3b6519dd80c2344db1c2f1ec0
t.uehara <t.uehara@gmail.com>
2023-08-21 22:52:14 +0900

    resist.jsにエラーハンドリングを追加

--- pages/api/resist.js
+++ pages/api/resist.js
@@ -13,12 +13,19 @@ export default async function handler(req, res) {
     fetch(url, requestOptions)
       .then(response => response.json())
       .then(data => {
+        if (!response.ok) {
+          console.error('response.ok:', response.ok);
+          console.error('esponse.status:', response.status);
+          console.error('esponse.statusText:', response.statusText);
+          throw new Error(response.statusText);
+        }
         console.log('Response data:', data);
+        return res.status(200).json({ "message":"Success" })
       })
       .catch(error => {
         console.error('Error:', error);
+        return res.status(200).json({ "message":error })
       });
 
-    return res.status(200).json({ "message":"Success" })
   }
   
